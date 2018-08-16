a480d5e8f694797fb4f899cd7cc274a720164f69
siddesh001 <siddesh.001@gmail.com>
2018-08-16 03:05:53 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,20 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+
+def rf_rfe(df):
+    X=df.drop('SalePrice',axis=1)
+    y=df.SalePrice
+    model=RandomForestClassifier()
+    rfe=RFE(model)
+    rfe=rfe.fit(X,y)
+    
+    a=pd.DataFrame()
+    a['rank']=rfe.ranking_
+    a['columns']=data.drop('SalePrice',axis=1).columns
+    result=(a.sort_values('rank')['columns'].head(int(a.shape[0]/2)))
+    result.sort_index(inplace=True)
+    
+    return result.tolist()
+
 
